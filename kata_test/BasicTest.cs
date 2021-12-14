dd49aabfa317eb509fe664e9519d895068625187
Simon Holdsworth <holdsworth.simon@gmail.com>
2021-12-14 14:03:07 -0800

    working, maybe needs refactoring

--- kata_test/BasicTest.cs
+++ kata_test/BasicTest.cs
@@ -6,8 +6,30 @@ namespace kata_test;
 public class BasicTest
 {
     [Fact]
-    public void ReturnsZero()
+    public void FirstTest()
     {
-        Assert.Equal("0",Numerals.ToRoman(0));
+        Assert.Equal("i",Numerals.ToRoman(1));
+    }
+    
+    [Theory]
+    [InlineData(1, "i")]
+    [InlineData(2, "ii")]
+    [InlineData(3, "iii")]
+    [InlineData(4, "iv")]
+    [InlineData(5, "v")]
+    [InlineData(8, "viii")]
+    [InlineData(9, "ix")]
+    [InlineData(10, "x")]
+    [InlineData(15, "xv")]
+    [InlineData(18, "xviii")]
+    [InlineData(23, "xxiii")]
+    [InlineData(40, "xl")]
+    [InlineData(43, "xliii")]
+    [InlineData(45, "xlv")]
+    [InlineData(49, "xlix")]
+    [InlineData(53, "liii")]
+    public void TestOnes(int input, string expected)
+    {
+        Assert.Equal(expected,Numerals.ToRoman(input));
     }
 }
--- roman_numerals_kata/Numerals.cs
+++ roman_numerals_kata/Numerals.cs
@@ -1,10 +1,75 @@
-﻿namespace roman_numerals_kata;
+﻿using System.Net.Http.Headers;
+
+namespace roman_numerals_kata;
 
 public static class Numerals
 {
     public static string ToRoman(int input)
     {
-        return "0";
+        var returnString = "";
+        
+        returnString += ReturnNumber(input / 50, "l");
+        input = input % 50;
+        
+        if (input + 10 >= 50)
+        {
+            returnString += "xl";
+            input = input - 40;
+        }
+        
+        if (input + 5 >= 50)
+        {
+            returnString += "vl";
+            input = input - 45;
+        }
+
+        if (input + 1 == 50)
+        {
+            returnString += "il";
+            input = input - 49;
+        }
+        
+        
+        
+        returnString += ReturnNumber(input / 10, "x");
+        input = input % 10;
+
+        if (input + 1 == 10)
+        {
+            returnString += "ix";
+            input = input - 9;
+        }
+        
+        
+
+        returnString += ReturnNumber(input / 5, "v");
+        input = input % 5;
+        
+        
+        if (input + 1 == 5)
+        {
+            returnString += "iv";
+            input = input - 4;
+        }
+        
+        
+        
+        
+        returnString += ReturnNumber(input, "i");
+
+        return returnString;
+    }
+
+    private static string ReturnNumber(int input, string number)
+    {
+        var returnString = "";
+        for (int i = 0; i < input; i++)
+        {
+            returnString += number;
+        }
+
+        return returnString;
+        
     }
 
 }
