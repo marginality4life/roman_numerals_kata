cd9efb7fc3db29c937479b9a2b322d2c3adb3e69
Simon Holdsworth <holdsworth.simon@gmail.com>
2021-12-24 16:04:07 -0800

    completed soln

--- kata_test/BasicTest.cs
+++ kata_test/BasicTest.cs
@@ -5,9 +5,35 @@ namespace kata_test;
 
 public class BasicTest
 {
-    [Fact]
-    public void ReturnsZero()
+    [Theory]
+    [InlineData(new int[] {1,1, -1}, -1)]
+    [InlineData(new int[] {1, -1}, -1)]
+    [InlineData(new int[] {-1}, -1)]
+    public void ReturnsArrayProduct(int[] inputArray, int product)
     {
-        Assert.Equal("0",Numerals.ToRoman(0));
+        Assert.Equal(product,Numerals.GetProduct(inputArray));
     }
+
+    [Fact]
+    public void ReturnsLeftArraySlice()
+    {
+        var inputArray = new int[] {1, 2, 3};
+        Assert.Equal(new int[] {1,2}, Numerals.GetLeftArraySlice(new int[] {1,2,3}));
+    }
+
+    [Fact]
+    public void ReturnsRightArraySlice()
+    {
+        var inputArray = new int[] {1, 2, 3};
+        Assert.Equal(new int[] {2,3}, Numerals.GetRightArraySlice(new int[] {1,2,3}));
+    }
+    
+    [Theory]
+    [InlineData(new int[] {1, -1, -1, 1, 1, -1}, 5)]
+    [InlineData(new int[] {1, 1, -1, 1, 1}, 2)]
+    public void ReturnsLongestSubArrayLength(int[] inputArray, int length)
+    {
+        Assert.Equal(length,Numerals.GetLength(inputArray));
+    }
+    
 }
--- roman_numerals_kata/Numerals.cs
+++ roman_numerals_kata/Numerals.cs
@@ -2,9 +2,56 @@
 
 public static class Numerals
 {
-    public static string ToRoman(int input)
+    public static int GetProduct(int[] inputArray)
     {
-        return "0";
+        var product = inputArray[0];
+        if (inputArray.Length > 1)
+        {
+            for (var i = 1; i < inputArray.Length; i++)
+            {
+                product = product * inputArray[i];
+            }
+        }
+
+        return product;
     }
 
-}
+    public static int GetLength(int[] inputArray)
+    {
+        if (GetProduct(inputArray) == 1)
+            return inputArray.Length;
+
+        if (inputArray.Length == 1 && GetProduct(inputArray) == -1)
+            return 0;
+
+        var leftArray = GetLeftArraySlice(inputArray);
+        var rightArray = GetRightArraySlice(inputArray);
+
+        var leftArrayLength = GetLength(leftArray);
+        var rightArrayLength = GetLength(rightArray);
+
+        if (leftArrayLength > rightArrayLength)
+            return leftArrayLength;
+        return rightArrayLength;
+    }
+
+    public static int[] GetLeftArraySlice(int[] inputArray)
+    {
+        var outputArray = new int[inputArray.Length - 1];
+        for (var i = 0; i < inputArray.Length - 1; i++)
+        {
+            outputArray[i] = inputArray[i];
+        }
+        return outputArray;
+    }
+
+    public static int[] GetRightArraySlice(int[] inputArray)
+    {
+        var outputArray = new int[inputArray.Length - 1];
+        for (var i = 1; i < inputArray.Length; i++)
+        {
+            outputArray[i-1] = inputArray[i];
+        }
+        return outputArray;
+    }
+} 
