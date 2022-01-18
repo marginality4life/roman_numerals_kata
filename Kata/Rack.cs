14e35ba63cc00c8c84e8b025cca1d2463181b023
Simon Holdsworth <holdsworth.simon@gmail.com>
2022-01-17 18:10:18 -0800

    Completed kata

---
+++ Kata/Rack.cs
@@ -0,0 +1,20 @@
+﻿namespace roman_numerals_kata;
+
+public class Rack
+{
+    public int[] Balls { get; private set; }
+
+    public Rack()
+    {
+        Balls = Array.Empty<int>();
+    }
+    
+    public void Add(int input)
+    {
+        var tempBalls = Balls.ToList();
+        
+        tempBalls.Add(input);
+        Balls = tempBalls.OrderBy(x => x).ToArray();
+    }
+
+}
--- Test/BasicTest.cs
+++ Test/BasicTest.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using Xunit;
 using roman_numerals_kata;
 
@@ -6,8 +8,33 @@ namespace kata_test;
 public class BasicTest
 {
     [Fact]
-    public void ReturnsZero()
+    public void ReturnsEmpty()
     {
-        Assert.Equal("0",Numerals.ToRoman(0));
+        var rack = new Rack();
+        Assert.Equal(new List<int>(), rack.Balls);
+    }
+
+    [Fact]
+    public void BallsAddWorks()
+    {
+        var rack = new Rack();
+        
+        rack.Add(3);
+        Assert.Equal(new List<int>{3}, rack.Balls);
+        
+        rack.Add(1);
+        Assert.Equal(new List<int>{1,3}, rack.Balls);
+        
+        rack.Add(8);
+        Assert.Equal(new List<int>{1,3,8}, rack.Balls);
+        
+        rack.Add(5);
+        Assert.Equal(new List<int>{1,3,5,8}, rack.Balls);
+        
+        rack.Add(4);
+        Assert.Equal(new List<int>{1,3,4,5,8}, rack.Balls);
+        
+        rack.Add(22);
+        Assert.Equal(new List<int>{1,3,4,5,8,22}, rack.Balls);
     }
 }
