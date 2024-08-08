48f0428a74f0705dba31acdf4e11b2a833f725bb
Simon Holdsworth <holdsworth.simon@gmail.com>
2024-08-08 13:24:37 -0700

    Dynamic programming fibbonaci approach

--- Kata/Numerals.cs
+++ Kata/Numerals.cs
@@ -1,10 +1,29 @@
 ﻿namespace roman_numerals_kata;
 
-public static class Numerals
+public class Numerals
 {
     public static string ToRoman(int input)
     {
         return "0";
+    }
+    
+    private Dictionary<int, int> FibCache = new Dictionary<int, int>();
+    
+    public int Fibbonacci(int input)
+    {
+        if (FibCache.ContainsKey(input))
+            return FibCache[input];
+        
+        if (input < 1)
+            return -1;
+        if (input == 1)
+            return 1;
+        if (input == 2)
+            return 1;
+        
+        var result = Fibbonacci(input - 1) + Fibbonacci(input - 2);
+        FibCache[input] = result;
+        return result;
     }
 
 }
--- Test/BasicTest.cs
+++ Test/BasicTest.cs
@@ -18,4 +18,16 @@ public class BasicTest
         Assert.Equal("0",Numerals.ToRoman(input));
     }
     
+   
+    [Fact]
+    public void Fibbonacci()
+    {
+        var numerals = new Numerals();
+        
+        Assert.Equal(1,numerals.Fibbonacci(1));
+        Assert.Equal(1,numerals.Fibbonacci(2));
+        Assert.Equal(2,numerals.Fibbonacci(3));
+        Assert.Equal(3,numerals.Fibbonacci(4));
+        Assert.Equal(5,numerals.Fibbonacci(5));
+    }
 }
